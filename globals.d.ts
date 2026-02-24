b98c3f27540ccfffa8d9da0455ac11e8fba3a1e5
Dewei Setiawan <dewei812@gmail.com>
2026-02-24 17:58:41 +0800

    Update globals.d.ts

--- globals.d.ts
+++ globals.d.ts
@@ -1,6 +1,15 @@
 declare global {
   interface Window {
     game?: any;
+    currentUser?: {
+      id: number;
+      google_id: string;
+      email?: string;
+      name?: string;
+      picture?: string;
+      display_name: string;
+      total_score: number;
+    };
   }
 }
 
